0fb953299c89318216589b2d35086456d9dc4768
Odera Mo <okonkwoodera@gmail.com>
2017-02-01 15:17:24 +0100

    Create alternative header for logged in admin

--- www/dashboard.php
+++ www/dashboard.php
@@ -6,9 +6,35 @@
 	include "includes/functions.php";
 
 	# import header
-	include "includes/header.php";
-
+	include "includes/header2.php";
 ?>
+
+
+<div class="wrapper">
+	<div id="stream">
+		<h1 id="register-label">Add Post</h1>
+		<hr>
+
+		<form id="register">
+		<div>
+			<label>post title:</label>
+			<input type="text" name="title" placeholder="Post Title">
+		</div>
+		<div>
+			<label>post excerpt:</label>
+			<textarea placeholder="summary" name ="summary"></textarea>
+		</div>
+
+		<div>
+			<label>post content:</label>
+			<textarea placeholder="content" name="content" class="post-box"></textarea>
+		</div>
+
+		<input type="submit" name="submit" value="publish">
+
+		</form>
+	</div>
+</div>
 
 <?php
 	# import footer
---
+++ www/includes/header2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Register</title>
+	<link rel="stylesheet" type="text/css" href="styles/styles.css">
+</head>
+<body>
+<section>
+	<div class="mast">
+		<h1>T<span>SSB</span></h1>
+		<nav>
+			<ul class="clearfix">
+				<li><a href="#" class="selected">add posts</a></li>
+				<li><a href="#">view posts</a></li>
+				<li><a href="#">view posts</a></li>
+				<li><a href="#">view posts</a></li>
+				<li><a href="#">logout</a></li>
+			</ul>
+		</nav>
+	</div>
+</section>
