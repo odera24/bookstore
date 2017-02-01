71e264e060a632032fe09b54590f4d5e939777e5
Odera Mo <okonkwoodera@gmail.com>
2017-02-01 15:17:24 +0100

    Create alternative header for logged in admin

--- www/dashboard.php
+++ www/dashboard.php
@@ -7,8 +7,34 @@
 
 	# import header
 	include "includes/header.php";
-
 ?>
+
+
+<div class="wrapper">
+	<div id="stream">
+		<h1 id="register-label">Add Post</h1>
+		<hr>
+
+		<form id="register">
+		<div>
+			<label>post title:</label>
+			<input type="text" name="title" placeholder="Post Title">
+		</div>
+		<div>
+			<label>post excerpt:</label>
+			<textarea placeholder="summary" name ="summary"></textarea>
+		</div>
+
+		<div>
+			<label>post content:</label>
+			<textarea placeholder="content" name="content" class="post-box"></textarea>
+		</div>
+
+		<input type="submit" name="submit" value="publish">
+
+		</form>
+	</div>
+</div>
 
 <?php
 	# import footer
